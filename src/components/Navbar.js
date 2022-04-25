9e4c3cea92629b2f49160465761af55f284d44c1
zadigg <62830716+zadigg@users.noreply.github.com>
2022-04-25 22:38:30 +0300

    home logo changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "../App.css";
 import { Link } from "react-router-dom";
+import logo from "./Images/homelogo.png";
+
 function Navbar() {
   return (
     <div className="Navbar_container">
       <div className="Navbar_body">
         <div>
           <Link to={"/"}>
-            <div>Home</div>
+            <img src={logo} width="220px" alt="" />
           </Link>
         </div>
         <div className="Navbar_innerbody">
